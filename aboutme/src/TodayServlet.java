d6b4f14eb2e11b11d55427cd1945f773cab23d95
Jeon Kyung Yoon <kyungyoon.jeon@nts-corp.com>
2020-10-22 17:39:33 +0900

    Refactor TodayServlet.java
    - 불필요한 공백 제거

--- aboutme/src/TodayServlet.java
+++ aboutme/src/TodayServlet.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
@@ -14,21 +13,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	public TodayServlet() {
-        super();
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public TodayServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.print("<a href='./index.html'>메인화면</a>");
-		
 		out.print("<h1 style='text-align:center;'>");
 		out.print("현재시간 : ");
-		out.print(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")).toString()+"<br>");
+		out.print(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")).toString() + "<br>");
 		out.print("</h1>");
 		out.close();
 	}
-
 }
