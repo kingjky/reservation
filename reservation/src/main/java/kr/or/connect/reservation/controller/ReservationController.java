66106c4b4f7b35fb4e02490dcab4073afcb0a812
Jeon Kyung Yoon <kyungyoon.jeon@nts-corp.com>
2020-11-13 19:59:13 +0900

    Refactor PJT3
    - Frontend
            1. Paging을 위한 list count를 더보기 button의 value로 이동
                    - google paging 참조
    - Backend
            1. DaoTest, ServiceTest를 src/test/java로 이동
            2. EntityController 추가
                    - 각 Controller의 super class로 지정
                    - Response에 ResponseEntity 적용
            3. ExceptionManager 추가
                    - ReservationException 추가
                    - @ExceptionHandler 사용
                    - NumberFormatException에 대한 예외 처리
    	4. ProductDao에서 @Transactional 제거
    		- SELECT query는 Transaction 처리 대상이 되지 않음
    	5. log4j Dependency 추가
    	6. Dependency Injection 수정
    		- Field Injection 에서 Constructor Injection 으로 변경

--- reservation/src/main/java/kr/or/connect/reservation/controller/ReservationController.java
+++ reservation/src/main/java/kr/or/connect/reservation/controller/ReservationController.java
@@ -6,16 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestScope;
 
 import kr.or.connect.reservation.dto.Promotion;
 import kr.or.connect.reservation.service.PromotionService;
 
 @Controller
+@RequestMapping(path = "/")
 public class ReservationController {
-	@Autowired
 	PromotionService promotionService;
-
-	@GetMapping(path = "/")
+	
+	@Autowired
+	public ReservationController(PromotionService promotionService) {
+		super();
+		this.promotionService = promotionService;
+	}
+	
+	@GetMapping
 	public String mainpage(ModelMap model) {
 		List<Promotion> promotions = promotionService.getPromotions();
 		model.addAttribute("promotions", promotions);
